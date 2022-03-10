607b5a189a34391f49efc324f9dea152b77e1332
김태영 <kimtaeyoung@0Taes-Mac.local>
2022-03-11 01:12:28 +0900

    이미지 정리, seo 작업

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,12 +1,35 @@
 import '@styles/globals.css';
 import type { AppProps } from 'next/app';
 import Layout from '@components/Layout';
+import { NextSeo } from 'next-seo';
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <Layout>
-      <Component {...pageProps} />
-    </Layout>
+    <>
+      <NextSeo
+        title='솔로 기장'
+        description='인연을 찾는 곳'
+        canonical='https://sologijang.vercel.app'
+        openGraph={{
+          url: 'https://sologijang.vercel.app',
+          title: '솔로 기장',
+          description: '인연을 찾는 곳',
+          images: [
+            {
+              url: 'https://sologijang.vercel.app/logo.jpg',
+              width: 800,
+              height: 600,
+              alt: '솔로 기장',
+              type: 'image/jpeg',
+            },
+          ],
+          site_name: '솔로 기장',
+        }}
+      />
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </>
   );
 }
 

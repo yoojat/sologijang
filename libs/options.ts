5c9e0e5e37ba9aaf75b87b521112d643742bf363
김태영 <kimtaeyoung@0Taes-Mac.local>
2022-03-10 23:24:51 +0900

    참여 방법 페이지 추가

--- libs/options.ts
+++ libs/options.ts
@@ -1,4 +1,7 @@
 export const bgColor = 'bg-teal-700';
 export const logo = '/logo.png';
 export const title = 'Website In Busan';
-export const menus = [{ name: '소개', path: '/introduce' }];
+export const menus = [
+  { name: '소개', path: '/introduce' },
+  { name: '참여 방법', path: '/join' },
+];

453f9a894975c6679ae9560f2499dbdf791ac11e
김태영 <kimtaeyoung@0Taes-Mac.local>
2022-03-15 20:22:25 +0900

    구글 애널리틱스 추가

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,7 +3,22 @@ import type { AppProps } from 'next/app';
 import Layout from '@components/Layout';
 import { NextSeo } from 'next-seo';
 
+import * as gtag from '@libs/gtag';
+import { useRouter } from 'next/router';
+import { useEffect } from 'react';
+
 function MyApp({ Component, pageProps }: AppProps) {
+  const router = useRouter();
+  useEffect(() => {
+    const handleRouteChange = (url: string) => {
+      gtag.pageview(url);
+    };
+    router.events.on('routeChangeComplete', handleRouteChange);
+    return () => {
+      router.events.off('routeChangeComplete', handleRouteChange);
+    };
+  }, [router.events]);
+
   return (
     <>
       <NextSeo
--- pages/_document.tsx
+++ pages/_document.tsx
@@ -6,6 +6,8 @@ import Document, {
   NextScript,
 } from 'next/document';
 
+import { GA_TRACKING_ID } from '@libs/gtag';
+
 class MyDocument extends Document {
   static async getInitialProps(ctx: DocumentContext) {
     const initialProps = await Document.getInitialProps(ctx);
@@ -26,6 +28,23 @@ class MyDocument extends Document {
           <link
             href='https://fonts.googleapis.com/css2?family=Noto+Sans+KR:wght@100;300;400;500;700;900&display=swap'
             rel='stylesheet'
+          />
+          <script
+            async
+            src={`https://www.googletagmanager.com/gtag/js?id=${GA_TRACKING_ID}`}
+          />
+          <script
+            dangerouslySetInnerHTML={{
+              __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+
+            gtag('config', '${GA_TRACKING_ID}', {
+              page_path: window.location.pathname,
+            });
+          `,
+            }}
           />
         </Head>
         <body>
